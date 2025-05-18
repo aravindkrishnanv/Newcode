b0913bc194525f5157c523cbdbe02b314a1aae14
aravindkrishnanv <aravindkrishnan190@gmail.com>
2025-05-18 20:26:50 +0530

    Commit1

---
+++ Hellp.py
@@ -0,0 +1,25 @@
+print("Hello World")
+
+message = "Hello python world!"
+print(message)
+
+message = 'Hello python world "Sublime text"'
+print(message)
+
+name = "aravind venugopal"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "aravind"
+last_name = "venugopal"
+full_name = f"{first_name}{last_name}"
+print(full_name)
+
+first_name = "aravind"
+last_name = "venugopal"
+full_name = f"Hello, {first_name.title()} {last_name.title()}"#Space and caps
+print(full_name)
+
+#comment
+import this#Zen of Python
